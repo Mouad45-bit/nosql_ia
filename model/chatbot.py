2a5c2feea8a34e1e24e5d17efb60a58431cf1ca4
mouad fekkak <mouadfekkak2002@gmail.com>
2025-11-21 00:01:34 +0100

    model -- chatbot - version2

--- model/chatbot.py
+++ model/chatbot.py
@@ -13,7 +13,7 @@ with open("embed_model_name.pkl", "rb") as f:
 
 embed_model = SentenceTransformer(model_name)
 
-
+##
 def retrieve(question: str, k: int = 5) -> pd.DataFrame:
     """Retourne les k lignes les plus proches pour une question."""
     q_emb = embed_model.encode(
@@ -24,17 +24,14 @@ def retrieve(question: str, k: int = 5) -> pd.DataFrame:
     D, I = index.search(q_emb, k)  # I = indices des lignes
     return df.iloc[I[0]]           # DataFrame de k lignes
 
-
+##
 def answer_question(question: str, k: int = 5) -> str:
     """Répond à la question en renvoyant la 'response' de la meilleure ligne."""
     retrieved = retrieve(question, k=k)
-
     # la meilleure ligne = première
     best = retrieved.iloc[0]
-
-    # Optionnel : tu peux aussi utiliser best["context"] si tu veux afficher le contexte
+    # 
     return best["response"]
-
 
 #########################
 print(answer_question("Who is the 2009 Dana Vikings Head Coach?"))
